4473166f191bdf50f7e84d11f6dc6eaf9fbbdbec
EKhoroshko <xapoxa14@gmail.com>
2021-07-14 18:34:39 +0300

    work home!!! ahtung

--- hero/src/app/app.module.ts
+++ hero/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -10,6 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroRegComponent } from './hero-reg/hero-reg.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,12 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     DashboardComponent,
     HeroSearchComponent,
+    HeroRegComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
---
+++ hero/src/app/hero-reg/hero-reg.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-hero-reg',
+  templateUrl: './hero-reg.component.html',
+  styleUrls: ['./hero-reg.component.css']
+})
+
+export class HeroRegComponent implements OnInit {
+  heroForm: FormGroup;
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+   
+  onSubmit(){
+    const controls = this.heroForm.controls;
+
+    if(this.heroForm.invalid){
+      Object.keys(controls)
+      .forEach(controlName => controls[controlName]
+      .markAsTouched());
+
+      return;
+    }
+
+    console.log(this.heroForm.value);
+  }
+
+  /*isControlInvalid(controlName: string): boolean {
+    const control = this.heroForm.controls[controlName];
+
+    const result = control.invalid && control.touched;
+
+    return result;
+  }*/
+
+  private initForm() {
+    this.heroForm = this.fb.group({
+      name: ['', [
+        Validators.required
+      ]
+      ],
+      email: ['', [
+        Validators.required, Validators.email
+      ]
+      ]
+    });
+  }
+
+ 
+}
